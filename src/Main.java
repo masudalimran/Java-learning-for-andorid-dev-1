9195fdb56f812f78a8dc7fc27bb868a9ac559d1b
Masud Al Imran <masudalimran@Masuds-MacBook-Air.local>
2023-04-28 18:14:37 +0600

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Main {
 
     public static void main(String[] args) {
-        Person p = new Person();
-        p.setFirstName("Masud");
-        p.setLastName("Al Imran");
-        System.out.println(p.toString());
-        System.out.println("yay");
+        Person p1 = new Person("p1");
+        p1.setFirstName("Masud");
+        p1.setLastName("Al Imran");
+
+        Person p2 = new Person("p2");
+        p2.setFirstName("Saidul");
+        p2.setLastName("Islam");
+
+        Person p3 = new Person("p3");
+        p3.setFirstName("Azfar");
+        p3.setLastName("Ahmed");
+//        System.out.println(p.toString());
+
+        List<Person> persons = new ArrayList<>();
+        persons.add(p1);
+        persons.add(p2);
+        persons.add(p3);
+
+        String findResult = "No Match Found";
+        if (Person.findPerson(persons, p3) != null) {
+            findResult = Objects.requireNonNull(Person.findPerson(persons, p3)).toString();
+        }
+
+        for (Person person :
+                persons) {
+            System.out.println(person.getPersonInfo());
+        }
+
+        System.out.println("------------------------------------------\nSearch Result");
+        System.out.println(findResult);
+
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,11 +1,16 @@
+import java.util.List;
 import java.util.Random;
 
 public class Person {
     Random rand = new Random();
 
-    private String id = "Guest" + rand.nextInt(100000, 999999);
+    private String id;
     private String firstName;
     private String lastName;
+
+    public Person(String s) {
+        id = s + rand.nextInt(100000, 999999);
+    }
 
     public String getPersonId() {
         return id;
@@ -17,7 +22,7 @@ public class Person {
 
     public void setFirstName(String fname) {
         firstName = fname;
-        id = fname.toLowerCase().strip() + rand.nextInt(100000, 999999);
+//        id = fname.toLowerCase().strip() + rand.nextInt(100000, 999999);
     }
 
     public String getLastName() {
@@ -34,6 +39,21 @@ public class Person {
 
     public String getPersonInfo() {
         return "ID: " + id + "\n" + "Full Name: " + getFullName() + "\n" + "First Name: " + firstName + "\n" + "Last Name: " + lastName + "\n";
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + "\n" + "Full Name: " + getFullName() + "\n" + "First Name: " + firstName + "\n" + "Last Name: " + lastName + "\n";
+    }
+
+    public static Person findPerson(List<Person> persons, Person p) {
+        for (Person person :
+                persons) {
+            if (person.equals(p)) {
+                return person;
+            }
+        }
+        return null;
     }
 
 
